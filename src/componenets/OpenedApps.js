9138e6582eb25cd13767a4bb8fa31be0475c364d
mostafadisha <mostafa.abdelkareem@hotmail.com>
2019-10-09 18:04:19 +1100

    updates

--- src/componenets/OpenedApps.js
+++ src/componenets/OpenedApps.js
@@ -21,22 +21,22 @@ class OpenedApps extends Component {
           </i>
         </label>
 
-        <a href="#" class="menu-item blue">
+        <a href="#" class="menu-item blue md-trigger" data-modal="modal-2">
           <img src={require(`../assets/images/definde_act.svg`)} alt="" />
         </a>
-        <a href="#" class="menu-item green">
+        <a href="#" class="menu-item green md-trigger" data-modal="modal-3">
           <img src={require(`../assets/images/experiments.svg`)} alt="" />
         </a>
-        <a href="#" class="menu-item red">
+        <a href="#" class="menu-item red md-trigger" data-modal="modal-5">
           <img src={require(`../assets/images/dashboard.svg`)} alt="" />
         </a>
-        <a href="#" class="menu-item purple">
+        <a href="#" class="menu-item purple md-trigger" data-modal="modal-7">
           <img src={require(`../assets/images/reports.svg`)} alt="" />
         </a>
-        <a href="#" class="menu-item orange">
+        <a href="#" class="menu-item orange md-trigger" data-modal="modal-8">
           <img src={require(`../assets/images/treatment.svg`)} alt="" />
         </a>
-        <a href="#" class="menu-item lightblue">
+        <a href="#" class="menu-item lightblue md-trigger" data-modal="modal-10">
           <img src={require(`../assets/images/field_map.svg`)} alt="" />
         </a>
       </nav>
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -5,76 +5,76 @@ import cardsData from "../data/cardData.json";
 import SimpleStorage from "react-simple-storage";
 import OpenedApps from "../componenets/OpenedApps";
 export default class Category extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: "",
-      appCards: cardsData
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: "",
+            appCards: cardsData
+        };
+    }
 
-  render() {
-    var status = {
-      wip: [],
-      complete: []
-    };
+    render() {
+        var status = {
+            wip: [],
+            complete: []
+        };
 
-    const Apps = this.state.appCards.map(appInfo => (
-      <AppHolder appInfo={appInfo}></AppHolder>
-    ));
-    /*  */
-    let searchedCards = this.state.appCards;
-    const groupBy = (array, key) => {
-      return array.reduce((result, currentValue) => {
-        (result[currentValue[key]] = result[currentValue[key]] || []).push(
-          currentValue
-        );
-        return result;
-      }, {});
-    };
+        const Apps = this.state.appCards.map(appInfo => (
+            <AppHolder appInfo={appInfo}></AppHolder>
+        ));
+        /*  */
+        let searchedCards = this.state.appCards;
+        const groupBy = (array, key) => {
+            return array.reduce((result, currentValue) => {
+                (result[currentValue[key]] = result[currentValue[key]] || []).push(
+                    currentValue
+                );
+                return result;
+            }, {});
+        };
 
-    const cardsByCategory = groupBy(searchedCards, "appCategory");
-    let hideGroupTitle;
-    hideGroupTitle = "group-title show";
-    Object.keys(cardsByCategory).map(function(catKey) {
-      let groupTitle = (
-        <div className={hideGroupTitle}>
-          <h2>{catKey}</h2>
-        </div>
-      );
-
-      // status["complete"].push(groupTitle);
-      cardsByCategory[catKey].map(function(card) {
-        if (card.appCategory === catKey) {
-          if (status[card.category].indexOf(groupTitle) === -1) {
-            status[card.category].push(groupTitle);
-          }
-          status[card.category].push(
-            <React.Fragment>
-              <div className="grid__item">
-                <div key={card.id}>
-                  <Card cardinfo={card}></Card>
+        const cardsByCategory = groupBy(searchedCards, "appCategory");
+        let hideGroupTitle;
+        hideGroupTitle = "group-title show";
+        Object.keys(cardsByCategory).map(function (catKey) {
+            let groupTitle = (
+                <div className={hideGroupTitle}>
+                    <h2>{catKey}</h2>
                 </div>
-              </div>
-            </React.Fragment>
-          );
-        }
-      });
-    });
-    return (
-      <main>
-        <SimpleStorage parent={this} />
-        {Apps}
-        <div className="container-drag color-change-2x">
-          <nav role="navigation"></nav>
+            );
 
-          <div className="droppable by-category">
-            <h3 className="page-title">Apps Grouped by Category</h3>
-            <div className="grid">{status.complete}</div>
-          </div>
-        </div>
-        <OpenedApps></OpenedApps>
-      </main>
-    );
-  }
+            // status["complete"].push(groupTitle);
+            cardsByCategory[catKey].map(function (card) {
+                if (card.appCategory === catKey) {
+                    if (status[card.category].indexOf(groupTitle) === -1) {
+                        status[card.category].push(groupTitle);
+                    }
+                    status[card.category].push(
+                        <React.Fragment>
+                            <div className="grid__item">
+                                <div key={card.id}>
+                                    <Card cardinfo={card}></Card>
+                                </div>
+                            </div>
+                        </React.Fragment>
+                    );
+                }
+            });
+        });
+        return (
+            <main>
+                <SimpleStorage parent={this} />
+                {Apps}
+                <div className="container-drag color-change-2x">
+                    <nav role="navigation"></nav>
+
+                    <div className="droppable by-category">
+                        <h3 className="page-title">Apps Grouped by Category</h3>
+                        <div className="grid">{status.complete}</div>
+                    </div>
+                </div>
+                <OpenedApps></OpenedApps>
+            </main>
+        );
+    }
 }
